b8be45933ed5db51d54c70648b587bcfe757210f
Tomislav Nikic <tomislav.nikic@fhwn.ac.at>
2022-10-24 16:30:00 +0200

    change settings
    Just making it look a bit more like the normal dmenu.

--- config.h
+++ config.h
@@ -5,17 +5,17 @@ static int bottom = 0;
 static int embedded = 0;
 static int minpwlen = 32;
 static int mon = -1;
-static int lineheight = 0;
-static int min_lineheight = 8;
+static int lineheight = 35;
+static int min_lineheight = 35;
 
 static const char *asterisk = "*";
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrains Mono:size=14"
 };
 static const char *prompt = NULL;
 static const char *colors[SchemeLast][4] = {
 	[SchemePrompt] = { "#bbbbbb", "#222222" },
-	[SchemeNormal] = { "#bbbbbb", "#222222" },
-	[SchemeSelect] = { "#eeeeee", "#005577" },
-	[SchemeDesc]   = { "#bbbbbb", "#222222" }
+	[SchemeNormal] = { "#e0def4", "#232136" },
+	[SchemeSelect] = { "#232136", "#c4a7e7" },
+	[SchemeDesc]   = { "#000000", "#00ffff" },
 };
